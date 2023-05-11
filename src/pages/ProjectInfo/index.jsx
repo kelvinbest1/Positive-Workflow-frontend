af29bf329ed3e0b3cfe9d645e7d8322c3a11634f
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-11 16:20:25 -0400

    add Members page

--- src/pages/ProjectInfo/index.jsx
+++ src/pages/ProjectInfo/index.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import {useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { GetProjectById } from "../../apicalls/projects";
-import { GetAllTasks } from "../../apicalls/tasks";
+
 import Divider from "../../components/Divider";
 
 import { getDateFormat } from "../../utils/helpers";
