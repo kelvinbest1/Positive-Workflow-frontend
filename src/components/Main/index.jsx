17e38d2b4ab86ae2dd441ff2b6f0c289001525ce
kelvinbest1 <kelvinlanier@yahoo.com>
2023-05-11 10:39:59 -0400

    added home route

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Auth from '../../pages/Auth'
 
-export default function Main(props){
+export default function Main(props) {
     return (
-    <main>
-    <BrowserRouter>
-        <Routes>                
-            
-            <Route path="/auth" element={<Auth/>} />
-            
-        </Routes>
-        </BrowserRouter>
-    </main>
+        <main>
+            <BrowserRouter>
+                <Routes>
+
+
+
+                    <Route path="/" element={<Home />} />
+                    <Route path="/auth" element={<Auth />} />
+
+                </Routes>
+            </BrowserRouter>
+        </main>
     )
 }
